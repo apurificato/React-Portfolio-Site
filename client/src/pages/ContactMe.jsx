700f736b854e1c7b50367fbb9527593d3b45fef8
Tony Stark <apurificato5680@gmai.com>
2024-05-23 10:29:09 -0400

    created contact form. created form/button for resume section. tweaked some styling in css file to get styles that I wanted for both pages.

--- client/src/pages/ContactMe.jsx
+++ client/src/pages/ContactMe.jsx
@@ -5,13 +5,24 @@ function ContactMe() {
         <h2>Contact Me</h2>
         <h3>Reach Out to Me So We Can Build Something Cool Together.</h3>
         <div>
-            <form>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <a href="">
-                    <button type="button">Submit Form</button>
-                </a>
+            <form action="/form-submitted" method="post">
+                <ul>
+                    <li>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="user_name" />
+                    </li>
+                    <li>
+                        <label htmlFor="mail">Email:</label>
+                        <input type="email" id="mail" name="user_email" />
+                    </li>
+                    <li>
+                        <label htmlFor="msg">Message:</label>
+                        <textarea id="msg" name="user_message"></textarea>
+                    </li>
+                    <li className="button">
+                        <button type="submit">Send your message</button>
+                    </li>
+                </ul>
             </form>
         </div>
     </section>
--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -4,7 +4,13 @@ function Resume() {
     <section id="resume">
         <h2>My Resume</h2>
         <h3>Click to Download and Save My Resume For Your Reference</h3>
+        <div id="download-section">
+            <form method="get" action="/Anthony-Purificato-Web-Developer-Resume-2024.pdf">
+                <button type="submit">Download Resume</button>
+            </form>
+        </div>
     </section>
+
     )
   }
   
